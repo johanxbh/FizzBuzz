ea65a4f269442113a07360d9b09d7fc45a93a20d
Johan <johan.xuan@mail.utoronto.ca>
2023-09-11 18:40:04 -0400

    A new java class called Main

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] i ;
+        i = new String[] {"a"};
+        fizzbuzz2.doFizzBuzz(5);
+        FizzBuzz.main(i);
+    }
+}
