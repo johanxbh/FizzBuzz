a32352592d0fd6976c4928c67cc807507ab482ff
Johan <johan.xuan@mail.utoronto.ca>
2023-09-11 18:20:49 -0400

    the thing I do for the graphic usage of git feature in intllij

--- src/fizzbuzz2.java
+++ src/fizzbuzz2.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class fizzbuzz2 {
+public class fizzbuzz2 {
+    public static void main(String[] args){
+        int j = 1;
+        while(j <= 100){
+            j = doFizzBuzz(j);
+        }
+    }
+
+    private static int doFizzBuzz(int j) {
+        boolean dividedby3 = j % 3 == 0;
+        boolean dividedby5 = j % 5 == 0;
+        if(dividedby3 && dividedby5){
+            System.out.println("FizzBuzz");
+        }else if(dividedby3){
+            System.out.println("Fizz");
+        } else if (dividedby5) {
+            System.out.println("Buzz");
+        }else{
+            System.out.println(j);
+        }
+        j++;
+        return j;
+    }
 }
